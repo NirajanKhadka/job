2995a9c75b0bc40ff07397e84269049ee81f8a97
NirajanKhadka <knirajan1011@gmail.com>
2023-03-13 14:29:29 +0545

    added logo.js and components folder

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage';
+import {Logo} from '../components'
 
 function Landing() {
   return (
     <Wrapper>
     <main>
       <nav>
-        <img src={logo} alt="logo" className='logo' />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
