52ec151d0abf933b2c7292e5185888c946944aec
NirajanKhadka <knirajan1011@gmail.com>
2023-03-15 13:54:52 +0545

    dashboard setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-import {Register,Landing,Error,Dashboard} from "./Pages";
+import {Register,Landing,Error} from "./Pages";
+import {SharedLayout, AddJob, AllJobs, Stats, Profile} from './Pages/dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element = {<Dashboard />} />
+
+        <Route path='/' element = {<SharedLayout />} >
+          <Route index element = {<Stats />} />
+          <Route path='add-job' element = {<AddJob />} />
+          <Route path='all-jobs' element = {<AllJobs />} />
+          <Route path='profile' element = {<Profile />} />
+        </Route>
+
         <Route path='landing' element = {<Landing />} />
         <Route path='register' element = {<Register />} />
         <Route path='*' element = {<Error />} />
